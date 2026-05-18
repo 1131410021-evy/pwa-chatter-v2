bde64c7867b630b6f6f6f2c27e6b331e270e3502
1131410021-evy <1131410021@mail.hwu.edu.tw>
2026-05-18 22:04:38 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login(): return render_template('login.html')
+
+@app.route('/register')
+def register(): return render_template('register.html')
+
+@app.route('/board')
+def board(): return render_template('board.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
